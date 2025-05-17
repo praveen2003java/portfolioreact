2d62845b8a7abad7b030f2703bef7c1d6dc2cdbb
praveen2003java <praveenbe2003@gmail.com>
2025-05-17 09:01:57 +0530

    FILE UPLOAD

--- protfolio-react/vite.config.js
+++ protfolio-react/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/protflioreact/',
+  base: '/portfolioreact/', // your repo name
   plugins: [react()],
 });
